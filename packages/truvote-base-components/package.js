a9203f62a3528f94f25ad03d246f42df8d2d02bf
Todd Dunning <todd@dunningmarketing.com>
2016-09-13 21:05:30 -0700

    Added truvote-base-components

---
+++ packages/truvote-base-components/package.js
@@ -0,0 +1,28 @@
+Package.describe({
+  name: "truvote-base-components",
+  summary: "Tru.Vote components package",
+  version: "0.0.1",
+});
+
+Package.onUse(function (api) {
+
+  api.versionsFrom(['METEOR@1.0']);
+
+  api.use([
+    // Nova packages
+    'nova:core@0.27.0-nova',
+    'nova:posts@0.27.0-nova',
+    'nova:users@0.27.0-nova',
+    'nova:comments@0.27.0-nova',
+
+    // third-party packages
+    'fortawesome:fontawesome@4.5.0',
+    'tmeasday:check-npm-versions@0.3.1',
+    'std:accounts-ui@1.2.6',
+    'utilities:react-list-container@0.1.10',
+  ]);
+
+  api.mainModule("lib/server.js", "server");
+  api.mainModule("lib/client.js", "client");
+  
+});
